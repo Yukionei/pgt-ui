7a0fce97cd9a7809f730d2a992c45e4086d7f26b
Yukionei <142705809+Yukionei@users.noreply.github.com>
2024-09-12 12:31:19 +1000

    Initial commit with UI files

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("button1").addEventListener("click", function() {
+    // Logic for GPT interaction goes here
+    document.getElementById("result").innerHTML = "Button 1 Clicked!";
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    // Logic for GPT interaction goes here
+    document.getElementById("result").innerHTML = "Button 2 Clicked!";
+});
